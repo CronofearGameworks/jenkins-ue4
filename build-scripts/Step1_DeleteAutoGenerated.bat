573e2b73655836951c6ebb925e2954fc4ac3d558
Kevin Yabar Garces <34262542+CronofearGameworks@users.noreply.github.com>
2018-06-20 22:55:51 -0500

    Delete Step1_DeleteAutoGenerated.bat

--- build-scripts/Step1_DeleteAutoGenerated.bat
+++
@@ -1,5 +0,0 @@
-if exist C:\ProjectName\_JenkinsCICD\Project\.vs rmdir /s /Q C:\ProjectName\_JenkinsCICD\Project\.vs
-if exist C:\ProjectName\_JenkinsCICD\Project\Intermediate rmdir /s /Q C:\ProjectName\_JenkinsCICD\Project\Intermediate
-if exist C:\ProjectName\_JenkinsCICD\Project\Binaries rmdir /s /Q C:\ProjectName\_JenkinsCICD\Project\Binaries
-if exist C:\ProjectName\_JenkinsCICD\Project\Saved rmdir /s /Q C:\ProjectName\_JenkinsCICD\Project\Saved
-if exist C:\ProjectName\_JenkinsCICD\Project\Build rmdir /s /Q C:\ProjectName\_JenkinsCICD\Project\Build
