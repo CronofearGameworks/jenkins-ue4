935cd5d57f22bf187cc922555cbdd6c0aca61966
Kevin Yabar Garces <34262542+CronofearGameworks@users.noreply.github.com>
2018-06-25 01:35:08 -0500

    Update Step7_Archive.bat

--- build-scripts/Step7_Archive.bat
+++ build-scripts/Step7_Archive.bat
@@ -1,3 +1,3 @@
 set "str=%~1"
-cd C:\_ProjectNil\_JenkinsCICD\Builds
-"C:\Program Files\7-Zip\7z.exe" a Project_Nil_%str%.zip C:\ProjectName\_JenkinsCICD\Builds\WindowsNoEditor\*
+cd C:\ProjectName\_JenkinsCICD\Builds
+"C:\Program Files\7-Zip\7z.exe" a ProjectName%str%.zip C:\ProjectName\_JenkinsCICD\Builds\WindowsNoEditor\*
