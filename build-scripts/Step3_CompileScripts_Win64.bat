606940b5c5b1c1bb04ef954a35e7f42122ee5651
Kevin Yabar Garces <34262542+CronofearGameworks@users.noreply.github.com>
2020-01-13 23:12:33 -0500

    Update Step3_CompileScripts_Win64.bat
    Added Name for better understanding.

--- build-scripts/Step3_CompileScripts_Win64.bat
+++ build-scripts/Step3_CompileScripts_Win64.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_4.19\Engine\Build\BatchFiles\Build.bat" ProjectEditor Win64 development "C:\ProjectName\_JenkinsCICD\Project\Project.uproject" -waitmutex
+"C:\Program Files\Epic Games\UE_4.19\Engine\Build\BatchFiles\Build.bat" ProjectNameEditor Win64 development "C:\ProjectName\_JenkinsCICD\Project\Project.uproject" -waitmutex
